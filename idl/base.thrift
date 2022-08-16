5b78f3ed132c3a943b2c210f4262c8d5fa3dc1fe
YangruiEmma <yangr.emma@gmail.com>
2022-08-16 22:33:17 +0800

    feat: add base.thrift

--- idl/base.thrift
+++ idl/base.thrift
@@ -18,17 +18,11 @@ namespace py base
 namespace go base
 namespace java com.cloudwego.thrift.base
 
-struct TrafficEnv {
-    1: bool Open = false,
-    2: string Env = "",
-}
-
 struct Base {
     1: string LogID = "",
     2: string Caller = "",
     3: string Addr = "",
     4: string Client = "",
-    5: optional TrafficEnv TrafficEnv,
     6: optional map<string, string> Extra,
 }
 
