59846154a2e5caaa617ae06a0a1edf3df21b9c8d
Marina-Sakai <marina.sakai@bytedance.com>
2024-03-21 17:13:32 +0800

    test: add multi-service test using netpoll mux

--- pbrpc/multiservicecall/multi_service_test.go
+++ pbrpc/multiservicecall/multi_service_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package multi_service
+package multiservicecall
 
 import (
 	"context"
@@ -21,15 +21,14 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cloudwego/kitex/client"
-	"github.com/cloudwego/kitex/server"
-	"github.com/cloudwego/kitex/transport"
-
 	"github.com/cloudwego/kitex-tests/kitex_gen/protobuf/pb_multi_service"
 	"github.com/cloudwego/kitex-tests/kitex_gen/protobuf/pb_multi_service/servicea"
 	"github.com/cloudwego/kitex-tests/kitex_gen/protobuf/pb_multi_service/serviceb"
 	"github.com/cloudwego/kitex-tests/kitex_gen/protobuf/pb_multi_service/servicec"
 	"github.com/cloudwego/kitex-tests/pkg/test"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/transmeta"
+	"github.com/cloudwego/kitex/server"
 )
 
 type ServiceAHandler struct{}
@@ -59,9 +58,32 @@ func GetServer(hostport string) server.Server {
 	return server.NewServer(server.WithServiceAddr(addr))
 }
 
+func GetMuxServer(hostport string) server.Server {
+	addr, _ := net.ResolveTCPAddr("tcp", hostport)
+
+	return server.NewServer(server.WithServiceAddr(addr), server.WithMuxTransport())
+}
+
 func TestRegisterService(t *testing.T) {
-	ip := "localhost:9901"
-	svr := GetServer(ip)
+	testRegisterService(t, GetServer, "localhost:9901")
+}
+
+func TestMuxRegisterService(t *testing.T) {
+	testRegisterService(t, GetMuxServer, "localhost:9902")
+}
+
+func TestMultiService(t *testing.T) {
+	ip := "localhost:9903"
+	testMultiService(t, GetServer(ip), ip)
+}
+
+func TestMuxMultiService(t *testing.T) {
+	ip := "localhost:9904"
+	testMultiService(t, GetMuxServer(ip), ip)
+}
+
+func testRegisterService(t *testing.T, getServer func(ip string) server.Server, ip string) {
+	svr := getServer(ip)
 	err := servicea.RegisterService(svr, new(ServiceAHandler), server.WithFallbackService())
 	test.Assert(t, err == nil)
 	err = serviceb.RegisterService(svr, new(ServiceBHandler))
@@ -69,7 +91,7 @@ func TestRegisterService(t *testing.T) {
 	err = servicec.RegisterService(svr, new(ServiceCHandler))
 	test.Assert(t, err == nil)
 
-	svr = GetServer(ip)
+	svr = getServer(ip)
 	test.PanicAt(t, func() {
 		_ = servicea.RegisterService(svr, new(ServiceAHandler), server.WithFallbackService())
 		_ = serviceb.RegisterService(svr, new(ServiceBHandler), server.WithFallbackService())
@@ -80,7 +102,7 @@ func TestRegisterService(t *testing.T) {
 		return true
 	})
 
-	svr = GetServer(ip)
+	svr = getServer(ip)
 	err = servicea.RegisterService(svr, new(ServiceAHandler))
 	test.Assert(t, err == nil)
 	err = servicec.RegisterService(svr, new(ServiceCHandler))
@@ -90,9 +112,7 @@ func TestRegisterService(t *testing.T) {
 	test.Assert(t, err.Error() == "method name [Chat1] is conflicted between services but no fallback service is specified")
 }
 
-func TestMultiService(t *testing.T) {
-	ip := "localhost:9900"
-	svr := GetServer(ip)
+func testMultiService(t *testing.T, svr server.Server, ip string) {
 	servicea.RegisterService(svr, new(ServiceAHandler))
 	serviceb.RegisterService(svr, new(ServiceBHandler))
 	servicec.RegisterService(svr, new(ServiceCHandler), server.WithFallbackService())
@@ -102,25 +122,25 @@ func TestMultiService(t *testing.T) {
 	req := &pb_multi_service.Request{Name: "pb multi_service req"}
 
 	time.Sleep(time.Second)
-	clientA, err := servicea.NewClient("ServiceA", client.WithHostPorts(ip))
+	clientA, err := servicea.NewClient("ServiceA", client.WithMetaHandler(transmeta.ClientTTHeaderHandler), client.WithHostPorts(ip))
 	test.Assert(t, err == nil, err)
 	resp, err := clientA.Chat1(context.Background(), req)
 	test.Assert(t, err == nil, err)
 	test.Assert(t, resp.Message == "servicea Chat1")
 
-	clientAWithTTHeader, err := servicea.NewClient("ServiceA", client.WithTransportProtocol(transport.TTHeader), client.WithHostPorts(ip))
+	clientAWithoutMetaHandler, err := servicea.NewClient("ServiceA", client.WithHostPorts(ip))
 	test.Assert(t, err == nil, err)
-	resp, err = clientAWithTTHeader.Chat1(context.Background(), req)
+	resp, err = clientAWithoutMetaHandler.Chat1(context.Background(), req)
 	test.Assert(t, err == nil, err)
-	test.Assert(t, resp.Message == "servicea Chat1")
+	test.Assert(t, resp.Message == "servicec Chat1")
 
-	clientB, err := serviceb.NewClient("ServiceB", client.WithHostPorts(ip))
+	clientB, err := serviceb.NewClient("ServiceB", client.WithMetaHandler(transmeta.ClientTTHeaderHandler), client.WithHostPorts(ip))
 	test.Assert(t, err == nil, err)
 	resp, err = clientB.Chat2(context.Background(), req)
 	test.Assert(t, err == nil, err)
 	test.Assert(t, resp.Message == "serviceb Chat2")
 
-	clientC, err := servicec.NewClient("ServiceC", client.WithHostPorts(ip))
+	clientC, err := servicec.NewClient("ServiceC", client.WithMetaHandler(transmeta.ClientTTHeaderHandler), client.WithHostPorts(ip))
 	test.Assert(t, err == nil, err)
 	resp, err = clientC.Chat1(context.Background(), req)
 	test.Assert(t, err == nil, err)
