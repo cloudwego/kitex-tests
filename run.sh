113d569f6e1bb52dc2abe4a0adda04561a5db9a8
jayantxie <xiezhengyao@bytedance.com>
2025-02-13 15:15:15 +0800

    feat: streamx integration tests

--- run.sh
+++ run.sh
@@ -140,6 +140,8 @@ protoc --go-grpc_out=grpc_gen/. ./idl/grpc_demo_2.proto
 
 # generate thrift streaming code
 LOCAL_REPO=$LOCAL_REPO ./thrift_streaming/generate.sh
+# generate thrift streaming code with streamx mode
+cd ./streamx && rm -rf ./kitex_gen && kitex_cmd -streamx -I idl ./idl/echo.thrift && kitex_cmd -streamx -I idl ./idl/echo.proto && cd -
 
 echo -e "\ngenerating code for testing ... done\n"
 
