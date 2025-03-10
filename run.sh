cloudwego/kitex-tests

commit 7c1fb36a69d61c95032142f97057d5010112380b

feat: streamx integration tests (#105)

run.sh
@@ -140,6 +140,8 @@ protoc --go-grpc_out=grpc_gen/. ./idl/grpc_demo_2.proto
 
 # generate thrift streaming code
 LOCAL_REPO=$LOCAL_REPO ./thrift_streaming/generate.sh
+# generate thrift streaming code with streamx mode
+cd ./streamx && rm -rf ./kitex_gen && kitex_cmd -streamx -I idl ./idl/echo.thrift && kitex_cmd -streamx -I idl ./idl/echo.proto && cd -
 
 echo -e "\ngenerating code for testing ... done\n"
 
@@ -191,13 +193,15 @@ if [[ -n $LOCAL_REPO && -n $CI ]]; then
     # only generate coverage file in ci env
     # CI=true will be set by Github runner or you can set by yourself
     # It will be slower if generate code coverage
-    go test -failfast -covermode=atomic -coverprofile=${LOCAL_REPO}/coverage.txt -coverpkg=github.com/cloudwego/kitex/... $test_modules
+    go test -failfast -covermode=atomic -coverprofile=${LOCAL_REPO}/coverage.txt.tmp -coverpkg=github.com/cloudwego/kitex/... $test_modules
    if [[ "$OSTYPE" =~ ^darwin ]]; # remove `mode: atomic` line
    then
-       sed -i '' 1d ${LOCAL_REPO}/coverage.txt
+       sed -i '' 1d ${LOCAL_REPO}/coverage.txt.tmp
    else
-       sed -i '1d' ${LOCAL_REPO}/coverage.txt
+       sed -i '1d' ${LOCAL_REPO}/coverage.txt.tmp
    fi
+   cat ${LOCAL_REPO}/coverage.txt.tmp >> ${LOCAL_REPO}/coverage.txt
+   rm ${LOCAL_REPO}/coverage.txt.tmp
 else
     go test -failfast $test_modules
 fi

streamx/idl/echo.thrift
@@ -1,0 +1,21 @@
+namespace go echo
+
+struct EchoClientRequest {
+    1: string Message,
+}
+
+struct EchoClientResponse {
+    1: string Message,
+}
+
+service TestService {
+    EchoClientResponse PingPong(1: EchoClientRequest req)
+    EchoClientResponse Unary(1: EchoClientRequest req) (streaming.mode="unary"),
+    EchoClientResponse EchoBidi (1: EchoClientRequest req) (streaming.mode="bidirectional"),
+    EchoClientResponse EchoClient (1: EchoClientRequest req) (streaming.mode="client"),
+    EchoClientResponse EchoServer (1: EchoClientRequest req) (streaming.mode="server"),
+}
+
+service EmptyService {
+
+}

streamx/idl/echo.proto
@@ -1,0 +1,40 @@
+/*
+ * Copyright 2023 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+syntax = "proto3";
+
+option go_package = "echopb";
+
+package echopb;
+
+service TestService {
+  rpc Unary (EchoClientRequest) returns (EchoClientResponse) {}
+  rpc EchoBidi (stream EchoClientRequest) returns (stream EchoClientResponse){}
+  rpc EchoClient (stream EchoClientRequest) returns (EchoClientResponse){}
+  rpc EchoServer (EchoClientRequest) returns (stream EchoClientResponse){}
+}
+
+service EmptyService {
+
+}
+
+message EchoClientRequest {
+  string message = 1;
+}
+
+message EchoClientResponse {
+  string message = 1;
+}
+

streamx/tracer.go
@@ -1,0 +1,56 @@
+// Copyright 2025 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package streamx
+
+import (
+	"context"
+
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/pkg/stats"
+)
+
+func NewTracer() stats.Tracer {
+	return &tracer{}
+}
+
+type tracer struct {
+}
+
+func (t *tracer) Start(ctx context.Context) context.Context {
+	return ctx
+}
+
+func (t *tracer) Finish(ctx context.Context) {
+	return
+}
+
+func (t *tracer) ReportStreamEvent(ctx context.Context, ri rpcinfo.RPCInfo, event rpcinfo.Event) {
+	if event.Event() == stats.StreamSend {
+		if ri.Invocation().Extra("test_stream_send_event") == nil {
+			var count int
+			ri.Invocation().(rpcinfo.InvocationSetter).SetExtra("test_stream_send_event", &count)
+		}
+		count := ri.Invocation().Extra("test_stream_send_event").(*int)
+		*count++
+	} else {
+		if ri.Invocation().Extra("test_stream_recv_event") == nil {
+			var count int
+			ri.Invocation().(rpcinfo.InvocationSetter).SetExtra("test_stream_recv_event", &count)
+		}
+		count := ri.Invocation().Extra("test_stream_recv_event").(*int)
+		*count++
+	}
+	return
+}

commit a441142b43a5fe5707f5ee498c7580a22fce123a

fix: issues introduced by #105

run.sh
@@ -62,7 +62,8 @@ fi
 # then no need to query the remote develop branch multiple times for it.
 export KITEX_LATEST_VERSION=develop # default value if not set
 if [[ -z $LOCAL_REPO ]]; then
-    export KITEX_LATEST_VERSION=`go list -m github.com/cloudwego/kitex@develop | cut -d" " -f2`
+  # use GOPROXY=direct to fix proxy cache issues
+  export KITEX_LATEST_VERSION=`GOPROXY=direct go list -m github.com/cloudwego/kitex@develop | cut -d" " -f2`
 fi
 
 go_install() {

streamx/idl/echo.thrift
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace go echo
 
 struct EchoClientRequest {